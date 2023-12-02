b866226e2c9d8927c9c729357b846e217eb6acbf
Davi Arndt <daviarndt@MacBook-Air-de-Davi.local>
2023-12-02 00:34:41 -0300

    First Commit

---
+++ src/main/java/com/example/dailyspent/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.dailyspent;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello, World!";
+    }
+}
---
+++ src/test/java/com/example/dailyspent/DailyspentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyspent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyspentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
