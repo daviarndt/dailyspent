5d828476f4a3fa6a21854afda89d7b666fe62a00
Davi Arndt <daviarndt@MacBook-Air-de-Davi.local>
2023-12-12 17:46:46 -0300

    AuthenticationService and Repository - Get UserDetails

---
+++ src/main/java/com/example/dailyspent/utils/AuthenticationService.java
@@ -0,0 +1,21 @@
+package com.example.dailyspent.utils;
+
+import com.example.dailyspent.user.UserRepository;
+import com.example.dailyspent.utils.exceptions.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserDetails(username);
+    }
+}
