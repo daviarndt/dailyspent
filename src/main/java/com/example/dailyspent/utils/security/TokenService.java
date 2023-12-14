96fdb5068773c8464f741e4e93dc1acb7fbc4ec5
Davi Arndt <daviarndt@MacBook-Air-de-Davi.local>
2023-12-14 16:36:16 -0300

    Authenticate User - Block requests except SaveUser and Login

--- src/main/java/com/example/dailyspent/utils/security/TokenService.java
+++ src/main/java/com/example/dailyspent/utils/security/TokenService.java
@@ -3,6 +3,7 @@ package com.example.dailyspent.utils.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.dailyspent.user.UserModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Error when trying to generate token", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Dailyspent")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Invalid or expired JWT");
         }
     }
 }
