d8d3ba734be05c3e1cc0cb7d17121b8ba75d8310
Davi Arndt <daviarndt@MacBook-Air-de-Davi.local>
2023-12-14 15:50:04 -0300

    Recover JWT through Authorization Header

---
+++ src/main/java/com/example/dailyspent/utils/security/SecurityFilter.java
@@ -0,0 +1,30 @@
+package com.example.dailyspent.utils.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = recoverToken(request);
+        System.out.println(tokenJWT);
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("JWT not sent on Authorization Header");
+        }
+        return authorizationHeader.replace("Bearer ", "");
+    }
+}
