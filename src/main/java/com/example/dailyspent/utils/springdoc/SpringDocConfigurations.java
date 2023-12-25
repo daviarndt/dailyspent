82bf5e78a447bc376f3d329a3beb854b99db2e25
Davi Arndt <daviarndt@MacBook-Air-de-Davi.local>
2023-12-25 01:22:41 -0300

    Add Title, Description and Contact to Docs

--- src/main/java/com/example/dailyspent/utils/springdoc/SpringDocConfigurations.java
+++ src/main/java/com/example/dailyspent/utils/springdoc/SpringDocConfigurations.java
@@ -2,6 +2,8 @@ package com.example.dailyspent.utils.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +16,12 @@ public class SpringDocConfigurations {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title("DailySpent API")
+                        .description(
+                                "Back-End side of the DailySpent application, that allows users to register their daily expenses.")
+                        .contact(new Contact()
+                                .name("Davi Arndt")));
     }
-
-
 }
