ed17ed6bc4fb21a4507a526c88fdbc77621105af
Davi Arndt <daviarndt@MacBook-Air-de-Davi.local>
2023-12-26 17:54:27 -0300

    Add ExceptionHandler for MehotArgumentNotValidException

--- src/main/java/com/example/dailyspent/utils/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/dailyspent/utils/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.example.dailyspent.utils.exceptions;
 import com.example.dailyspent.utils.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -32,5 +36,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ExpenseNotFoundException.class)
     public ResponseEntity<ApiResponse> handleExpenseNotFoundException(ExpenseNotFoundException ex) {
         return new ResponseEntity<>(ApiResponse.exception(ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<FieldError> errors = ex.getFieldErrors();
+        return new ResponseEntity<>(errors.stream().map(ErrorsDataValidation::new).toList(), HttpStatus.BAD_REQUEST);
+    }
+
+    public record ErrorsDataValidation(String field, String message) {
+        public ErrorsDataValidation(FieldError fieldError) {
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
     }
 }
